15c2b4f905822e1071acb759d37c73e2ee989fe5
David Monberg Nielsen <monbergnielsen@outlook.com>
2019-05-14 09:41:21 +0200

    Update ultrasenslib.h

--- ultrasenslib.h
+++ ultrasenslib.h
@@ -1,7 +1,7 @@
 #include "Arduino.h"
 
 /*
-	ultrasenslib.h - Library intended for interfacing with the HC-SR04 Ultrasonic Sensor.
+	ultrasenslib - Library for interfacing with the HC-SR04 Ultrasonic Sensor.
 	Created by David M. Nielsen, May 15th, 2019 under the MIT license.
 */
 
