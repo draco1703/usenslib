180be8717a0544265c9ace73a8e2cbd492206a0b
David Monberg Nielsen <monbergnielsen@outlook.com>
2019-05-14 09:44:09 +0200

    Create usens.cpp

---
+++ usens.cpp
@@ -0,0 +1,11 @@
+/*
+	usens - Library for interfacing with the HC-SR04 Ultrasonic Sensor.
+	Created by David M. Nielsen, May 15th, 2019 under the MIT license.
+*/
+
+#include "Arduino.h"
+#include "usens.h"
+
+usens::usens(int trigPin, int echoPin){
+	/* something something */
+}
