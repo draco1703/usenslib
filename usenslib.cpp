d326eadb255751e75b8726fcb9a58162c53ad488
David-M-Nielsen <monbergnielsen@outlook.com>
2019-05-14 22:19:36 +0200

    impl. isBlocked() and distance()

--- usenslib.cpp
+++ usenslib.cpp
@@ -1,19 +1,46 @@
-/*
-	usens - Library for interfacing with the HC-SR04 Ultrasonic Sensor.
+/*	usens - Library for interfacing with the HC-SR04 Ultrasonic Sensor.
 	Created by David M. Nielsen, May 15th, 2019 under the MIT license.
 */
 
 #include "Arduino.h"
 #include "usenslib.h"
 
-Usens::Usens(unsigned char trigPin, unsigned char echoPin){
-	/* implement */
+#define speedOfSound 0.034 /* 340 m/s or 0.034 cm/microsecond */
+#define blockedDistance 30 /* figure out some distance in cm */ 
+Usens::Usens(unsigned char trigPin, unsigned char echoPin){i
+	pinMode(trigPin, OUTPUT);
+	pinmode(echoPin, INPUT);
+	_trigPin = trigPin;
+	_echoPin = echoPin;
+
+	Serial.begin(7200);
 }
 
+/* needs testing */
 bool Usens::isBlocked(){
-	/* implement */
+	unsigned float currDist = distance();
+
+	if(currDist <= blockedDistance){
+		return true;
+	} else {
+		return false;
+	}
 }
 
-float Usens::distance(){
-	/* implement */
+/* needs testing */
+unsigned float Usens::distance(){
+	/* clear out trigPin */
+	digitalWrite(_trigPin, LOW);
+	delayMicroseconds(2);
+	
+	digitalWrite(_trigPin, HIGH);
+	delayMicroseconds(10);
+	digitalWrite(_trigPin, LOW);
+
+	_dur = pulseIn(_echoPin, HIGH);
+
+	/* distance = timeToObject * speedOfSound*/
+	_dist = _dur * speedOfSound / 2;
+
+	return _dist;
 }
